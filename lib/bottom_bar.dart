73c67b18d7720b64d91b9133924f5592b53f5a40
Kushal Beniwal <kbeniwal2305@gmail.com>
2021-03-13 21:26:24 +0530

    added subpages

---
+++ lib/bottom_bar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      color: Colors.lightBlueAccent,
+      child: Center(
+        child: Text("Work in Progress"),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:my_portfolio/landing.dart';
+import 'package:my_portfolio/about_me.dart';
+import 'package:my_portfolio/education.dart';
+import 'package:my_portfolio/achievements.dart';
+import 'package:my_portfolio/skills.dart';
+import 'package:my_portfolio/contact.dart';
+import 'package:my_portfolio/bottom_bar.dart';
 
 void main() => runApp(MyApp());
 
@@ -28,15 +35,13 @@ class _RandomWordsState extends State<RandomWords> {
         scrollDirection: Axis.vertical,
         child: Column(
           children: <Widget>[
-            Container(
-              height: 800,
-            ),
-            BottomAppBar(
-              color: Colors.lightBlueAccent,
-              child: Center(
-                child: Text("Work in Progress"),
-              ),
-            ),
+            LandingPage(),
+            AboutMe(),
+            Education(),
+            Achievements(),
+            Skills(),
+            Contact(),
+            BottomBar(),
           ],
         ),
       ),
