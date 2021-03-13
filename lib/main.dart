26d3480da2af45e399128feaf2a57da8b5e90755
Kushal Beniwal <kbeniwal2305@gmail.com>
2021-03-13 18:46:29 +0530

    started working on portfolio(made it scrollable with a bottom app bar)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,7 +8,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Startup Name Generator",
       theme: ThemeData(
-        primaryColor: Colors.deepPurple,
+        primaryColor: Colors.lightBlue,
       ),
       home: RandomWords(),
     );
@@ -25,86 +24,108 @@ class _RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Startup name generator"),
-        actions: [
-          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
-        ],
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  final List<WordPair> _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = <WordPair>{};
-  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: EdgeInsets.all(16),
-      itemBuilder: (BuildContext _context, int i) {
-        if (i.isOdd) {
-          return Divider();
-        }
-        final int indx = i ~/ 2;
-        if (indx >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[indx]);
-      },
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final tiles = _saved.map(
-            (WordPair pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
-
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Saved Suggestions'),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 800,
             ),
-            body: ListView(children: divided),
-          );
-        },
+            BottomAppBar(
+              color: Colors.lightBlueAccent,
+              child: Center(
+                child: Text("Work in Progress"),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: Text("Startup name generator"),
+  //       actions: [
+  //         IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+  //       ],
+  //     ),
+  //     body: _buildSuggestions(),
+  //   );
+  // }
+
+  // final List<WordPair> _suggestions = <WordPair>[];
+  // final Set<WordPair> _saved = <WordPair>{};
+  // final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+
+  // Widget _buildSuggestions() {
+  //   return ListView.builder(
+  //     padding: EdgeInsets.all(16),
+  //     itemBuilder: (BuildContext _context, int i) {
+  //       if (i.isOdd) {
+  //         return Divider();
+  //       }
+  //       final int indx = i ~/ 2;
+  //       if (indx >= _suggestions.length) {
+  //         _suggestions.addAll(generateWordPairs().take(10));
+  //       }
+  //       return _buildRow(_suggestions[indx]);
+  //     },
+  //   );
+  // }
+
+  // Widget _buildRow(WordPair pair) {
+  //   final alreadySaved = _saved.contains(pair);
+  //   return ListTile(
+  //     title: Text(
+  //       pair.asPascalCase,
+  //       style: _biggerFont,
+  //     ),
+  //     trailing: Icon(
+  //       alreadySaved ? Icons.favorite : Icons.favorite_border,
+  //       color: alreadySaved ? Colors.red : null,
+  //     ),
+  //     onTap: () {
+  //       setState(() {
+  //         if (alreadySaved) {
+  //           _saved.remove(pair);
+  //         } else {
+  //           _saved.add(pair);
+  //         }
+  //       });
+  //     },
+  //   );
+  // }
+
+  // void _pushSaved() {
+  //   Navigator.of(context).push(
+  //     MaterialPageRoute<void>(
+  //       builder: (BuildContext context) {
+  //         final tiles = _saved.map(
+  //           (WordPair pair) {
+  //             return ListTile(
+  //               title: Text(
+  //                 pair.asPascalCase,
+  //                 style: _biggerFont,
+  //               ),
+  //             );
+  //           },
+  //         );
+  //         final divided = ListTile.divideTiles(
+  //           context: context,
+  //           tiles: tiles,
+  //         ).toList();
+
+  //         return Scaffold(
+  //           appBar: AppBar(
+  //             title: Text('Saved Suggestions'),
+  //           ),
+  //           body: ListView(children: divided),
+  //         );
+  //       },
+  //     ),
+  //   );
+  // }
 }
