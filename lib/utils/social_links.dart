8bcc6c45b568b04a23e04cb215409ad8f353adb7
Kushal Beniwal <kbeniwal2305@gmail.com>
2021-06-21 10:52:44 +0530

    add icons in place of images

--- lib/utils/social_links.dart
+++ lib/utils/social_links.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SocialLinks extends StatelessWidget {
   const SocialLinks({Key? key}) : super(key: key);
@@ -9,56 +10,54 @@ class SocialLinks extends StatelessWidget {
     return Container(
       height: 60,
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          SocialLinkImage(
-              img: 'https://github.com/KushalBeniwal/images/raw/main/email.png',
-              url: 'mailto:kbeniwal2305@gmail.com'),
-          Padding(padding: EdgeInsets.all(10)),
-          SocialLinkImage(
-              img:
-                  'https://github.com/KushalBeniwal/images/raw/main/linkedin.png',
-              url: 'https://linkedin.com/in/KushalBeniwal'),
-          Padding(padding: EdgeInsets.all(10)),
-          SocialLinkImage(
-              img:
-                  'https://github.com/KushalBeniwal/images/raw/main/github.png',
+          SocialLinkIcon(
+              icon: Icon(Icons.mail), url: 'mailto:kbeniwal2305@gmail.com'),
+          SocialLinkIcon(
+              icon: FaIcon(FontAwesomeIcons.github),
               url: 'https://github.com/KushalBeniwal'),
-          Padding(padding: EdgeInsets.all(10)),
-          SocialLinkImage(
-              img:
-                  'https://github.com/KushalBeniwal/images/raw/main/twitter.png',
-              url: 'https://twitter.com/__cielo_O'),
+          SocialLinkIcon(
+              icon: FaIcon(FontAwesomeIcons.linkedin),
+              url: 'https://linkedin.com/in/KushalBeniwal'),
+          SocialLinkIcon(
+              icon: FaIcon(FontAwesomeIcons.twitter),
+              url: 'https://twitter.com/__cielo_O')
         ],
       ),
     );
   }
 }
 
-class SocialLinkImage extends StatefulWidget {
+class SocialLinkIcon extends StatefulWidget {
   final url;
-  final img;
-  const SocialLinkImage({Key? key, @required this.img, @required this.url})
+  final icon;
+  const SocialLinkIcon({Key? key, @required this.icon, @required this.url})
       : super(key: key);
 
   @override
-  _SocialLinkImageState createState() => _SocialLinkImageState();
+  _SocialLinkIconState createState() => _SocialLinkIconState();
 }
 
-class _SocialLinkImageState extends State<SocialLinkImage> {
+class _SocialLinkIconState extends State<SocialLinkIcon> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: InkWell(
-      onTap: _launchURL,
-      child: Container(
-        child: ClipRRect(
-          child: Image.network(widget.img, width: 30, height: 25),
-        ),
-      ),
-    ));
+    return IconButton(
+      onPressed: _launchURL,
+      icon: widget.icon,
+      focusColor: Colors.white,
+      hoverColor: Colors.white,
+      highlightColor: Colors.white,
+      splashColor: Colors.white,
+      disabledColor: Colors.grey,
+    );
   }
 
-  void _launchURL() async => await canLaunch(widget.url)
-      ? await launch(widget.url)
-      : throw 'Could not launch $widget.url';
+  _launchURL() async {
+    if (await canLaunch(widget.url)) {
+      await launch(widget.url);
+    } else {
+      throw 'Could not launch $widget.url';
+    }
+  }
 }
