c237acdf7100d7a841a6698ae70bb3c1291a170c
Kushal Beniwal <kbeniwal2305@gmail.com>
2021-07-02 16:53:54 +0530

    handle less width

--- lib/about_me.dart
+++ lib/about_me.dart
@@ -17,13 +17,20 @@ class _AboutMeState extends State<AboutMe> {
     double _width = MediaQuery.of(context).size.width;
     return Container(
       height: 600,
-      margin: EdgeInsets.only(top: 200, left: 50, right: 50),
-      child: Row(
+      margin: EdgeInsets.only(top: (_width > 1000) ? 200 : 70, left: (_width > 600) ? 50 : 10, right: (_width > 600) ? 50 : 10, bottom: (_width > 1000) ? 0 : 70),
+      child: (_width > 1000) ? Row(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Avatar(),
           Padding(padding: EdgeInsets.all((_width > 1200) ? 150 : 50)),
+          Info(),
+        ],
+      ) :
+      Column(
+        children: <Widget>[
+          Avatar(),
+          Padding(padding: EdgeInsets.all(15)),
           Info(),
         ],
       ),
